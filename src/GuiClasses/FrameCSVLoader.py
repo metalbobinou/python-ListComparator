14f9f0121521c0bd1fa7267615ade8f5e4d1f207
fabrice.boissier <fabrice.boissier@epita.fr>
2023-06-09 20:27:47 +0200

    Essai d'ajout de classes pour garder un même aspect dans les fenètres

--- src/GuiClasses/FrameCSVLoader.py
+++ src/GuiClasses/FrameCSVLoader.py
@@ -1,2 +1,96 @@
 import tkinter as tk
 from tkinter import ttk
+
+class FrameCSVLoader:
+    # Values we want to get from the user
+    Filename = tk.StringVar()
+    Separator = tk.StringVar()
+    Column = tk.StringVar()
+
+    # Canvas where to put the frame
+    Canvas = None
+    # Specifications of the frame
+    Geometry = None
+    Padding = None
+    BorderWidth = None
+    Relief = None
+    # Widgets of the frame
+    FileDescription = None
+    FileButton = None
+    FileField = None
+    SeparatorDescription = None
+    SeparatorField = None
+    ColumnDescription = None
+    ColumnField = None
+
+    def __init__(self, canvas):
+        # Get the canvas where to put the frame
+        self.Canvas = canvas
+
+        # Describe the field and button
+        self.FileDescription = tk.Label(self.Canvas,
+                                        text="CSV File Path:")
+        self.FileDescription.pack()
+        # Put the button for the file and fill the file field when chosen
+        self.FileButton = tk.Button(self.Canvas,
+                                    text="Choose file",
+                                    command=lambda: import_csv(self))
+        self.FileButton.pack()
+        # Create a file field
+        self.FileField = tk.Entry(self.Canvas, textvariable=Filename)
+        self.FileField.pack()
+
+        # Separator Description and Field
+        self.SeparatorDescription = tk.Label(self.Canvas,
+                                             text="Separator:")
+        self.SeparatorDescription.pack()
+        self.SeparatorField = tk.Entry(self.Canvas, textvariable=self.Separator)
+        self.SeparatorField.insert(0, ";")
+        self.SeparatorField.pack()
+
+        # Column Description and Field
+        self.ColumnDescription = tk.Label(self.Canvas,
+                                          text="Column:")
+        self.ColumnDescription.pack()
+        self.ColumnField = tk.Entry(self.Canvas, textvariable=Column)
+        self.ColumnField.insert(0, "6")
+        self.ColumnField.pack()
+
+    def GetFilename(self):
+        return (self.Filename)
+
+    def GetSeparator(self):
+        return (self.Separator)
+
+    def GetColumn(self):
+        return (self.Column)
+
+    def SetPadding(self, padding):
+        self.Padding = padding
+        self.Frame['padding'] = padding
+
+    def SetBorder(self, borderwidth):
+        self.BorderWidth = borderwidth
+        self.Frame['borderwidth'] = borderwidth
+
+    def SetRelief(self, relief):
+        self.Relief = relief
+        self.Frame['relief'] = relief
+
+    def ChooseFile(self):
+        # Ouvre une boîte de dialogue pour sélectionner un fichier CSV
+        file = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+
+        # Vérifie si un fichier a été sélectionné
+        if (file):
+            # Stocke le chemin du fichier
+            #file_path.set(file)
+            self.Filename.set(file)
+            self.FileField.insert(0, file)
+
+    def Validate(self):
+        # check the content of th cells ?... unsure about this one
+
+            
+def import_csv(TheFrame):
+    TheFrame.ChooseFile()
--- src/GuiClasses/WindowStart.py
+++ src/GuiClasses/WindowStart.py
@@ -1,3 +1,77 @@
 import tkinter as tk
 
-class StartWindows
+class StartWindow:
+    Geometry = "0"
+    Title = "List Window"
+    # Canvas getting the whole window
+    MainCanvas = None
+    # 1st Frame for CSV 1
+    FrameCSV1 = None
+    # 2nd Frame for CSV 2
+    FrameCSV2 = None
+    # Launch button
+    LaunchButton = None
+    
+    def __init__(self, geometry):
+        self.MainCanvas = tk.Tk()
+        self.SetGeometry(geometry)
+
+        # Add the CSV 1 frame
+        self.FrameCSV1 = FrameCSVLoader.FrameCSVLoader(self.MainCanvas)
+
+        # Add the CSV 2 frame
+        self.FrameCSV2 = FrameCSVLoader.FrameCSVLoader(self.MainCanvas)
+        
+        # Add the launch button
+        self.PutLaunchButton()
+
+
+    def PutLaunchButton(self):
+        self.LaunchButton = tk.Button(window,
+                                      text="Launch",
+                                      command=lambda: Launch_WindowListActions(self))
+        self.LaunchButton.pack()
+
+
+    def SetTitle(self, title):
+	self.Title = title
+        self.MainCanvas.title(title)
+
+    def SetGeometry(self, geometry):
+	self.Geometry = geometry
+        self.MainCanvas.geometry(geometry)
+        
+    def GetTitle(self):
+        return (self.Title)
+
+    def GetGeometry(self):
+        return (self.Geometry)
+
+    def CallWithdraw(self):
+        self.MainCanvas.withdraw()
+    
+    def CallDestroy(self):
+        self.MainCanvas.destroy()
+    
+    def GetCSVInfos(self, num):
+        if (num == 1):
+            return (self.FrameCSV1.Validate())
+        else if (num == 2):
+            return (self.FrameCSV2.Validate())
+        else:
+            print("Error in CSV Frame selection")
+            return (None)
+
+def Launch_WindowListAction(TheStartWindow):
+    # Get CSV 1 & 2 informations
+    CSV1Infos = TheStartWindow.GetCSVInfos(1)
+    CSV2Infos = TheStartWindow.GetCSVInfos(2)
+
+    ### TODO : !!! ADD VERIFICATIONS SOMEWHERE !!!
+    
+    # Close the main window
+    TheStartWindow.destroy()
+    # unsure about this one
+    
+    # Launch the WindowActions and the 2 WindowLists
+    # unsure about this one
