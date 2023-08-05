f54c72f5dbb1b2b68c7f51fdb517f6ac1ea0c5ae
Metalman <metalbobinou@users.noreply.github.com>
2023-08-05 19:08:03 +0200

    Reorganization bis [1]

--- plugins/example_plugin.py
+++ plugins/example_plugin.py
@@ -1,1 +1,24 @@
+from PluginLogic import PluginLogic
 
+# You can rename the class, but keep its name exactly like the file one
+class example_logic(PluginLogic):
+    # Name for calling this plugin
+    name_str = "NEW_PLUGIN"
+
+    # Text printed when calling the script with wrong parameters
+    help_str = "This is an example plugin that copy the 1st element of each " \
+        "list (if the list isn't empty)"
+
+    # Text of button in the GUI
+    button_str = "Example Plugin"
+
+    # The main function doing something with the lists and returning a list
+    def Logic(self, list1, list2):
+        out = []
+        if ((not (list1 is None)) and
+            (not (list2 is None))):
+            if (list1 != []):
+                out.append(list1[0])
+            if (list2 != []):
+                out.append(list2[0])
+        return (out)
